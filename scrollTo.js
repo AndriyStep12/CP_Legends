299a066d893a7d2c1bffb942120d975bcdaffd63
AndriyStep12 <andystep2008@gmail.com>
2022-09-03 13:10:05 +0300

    main

---
+++ scrollTo.js
@@ -0,0 +1,16 @@
+//function scrollTopAnimated() {
+//    $("html, body").animate({
+//        scrollTop: "820"
+//    }, 1000);
+//}
+
+
+//$(".down").click(() => {
+//    window.scrollTo(0, 1000);
+//
+//    // этот код меняет поведение прокрутки на "smooth"
+//    window.scrollTo({
+//        top: 1000,
+//        behavior: "smooth"
+//    });
+//});
